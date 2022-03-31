8b571f6bfd9833b378936589b1b810459810c458
Austin-Deccentric <caugust19.ac@gmail.com>
2022-03-31 14:59:13 +0100

    removed secrets

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,10 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "135280dfd0d5438490327eb11c37ff8d";
+require("dotenv").config();
+
+export const INFURA_ID = process.env.INFURA_ID;
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "ZYAXTQFZWAEU8XDRY9A61ZICWWWR8IFPH5";
+export const ETHERSCAN_ID = process.env.ETHERSCAN_ID;
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
