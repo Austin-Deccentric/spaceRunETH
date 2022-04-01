db6fbaa86820a5719c9e48373a36e7c2aa95ad52
Austin-Deccentric <caugust19.ac@gmail.com>
2022-04-01 18:02:43 +0100

    feature: implemented vendor contract.

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -2,22 +2,54 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./YourToken.sol";
+import "./atadiaToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+  atadiaToken public _atadiaToken;
+  uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+    _atadiaToken = atadiaToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    require( msg.value > 0, "Your ETH balance is low." );
+    uint256 tokenBuyAmount = msg.value * tokensPerEth;
+    
+    (bool sent) = _atadiaToken.transfer(msg.sender, tokenBuyAmount); 
+    require(sent, "Failed to complete sale");
+
+    emit BuyTokens(msg.sender, msg.value, tokenBuyAmount);
+    
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() external onlyOwner {
+    uint256 contractBalance = address(this).balance;
+    require(contractBalance > 0, "No FUNDS to transfer");
+
+    // transfer to owner
+    (bool sent,) = msg.sender.call{value: address(this).balance}("");
+    require(sent, "Failed to send Sale proceeds to owner");
+  }
 
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 amount) public {
+    require(_atadiaToken.balanceOf(msg.sender) >= amount, "You don not have enough Tokens.");
+    
+    _atadiaToken.transferFrom(msg.sender, address(this), amount);
+    uint256 ethToSend = amount / tokensPerEth;
+    
+    // transfer to user
+    (bool sent,) = msg.sender.call{value: ethToSend}("");
+    require(sent, "Failed to send ETH");
+
+    emit SellTokens(msg.sender, ethToSend, amount);
+  }
 
 }
