bd2c6bb89d3c6c922ffb64a6500cc6c4e10cea4a
Austin-Deccentric <caugust19.ac@gmail.com>
2022-04-01 21:00:50 +0100

    chore: removed unused variable

--- packages/hardhat/contracts/atadiaToken.sol
+++ packages/hardhat/contracts/atadiaToken.sol
@@ -5,11 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract atadiaToken is ERC20 {
-    address public owner;
-
     constructor() ERC20("Atadia", "ATA") {
         //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
-        owner = msg.sender;
-        _mint(msg.sender, 2000 * 10 ** 18);
+        _mint(msg.sender, 5000 * 10 ** 18);
     }
 }
