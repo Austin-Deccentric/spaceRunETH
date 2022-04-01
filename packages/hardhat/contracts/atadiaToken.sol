5866be32c42389e74918b746ba9378ee9e47418d
Austin-Deccentric <caugust19.ac@gmail.com>
2022-04-01 18:02:17 +0100

    feature: Implemented erc20 token

--- packages/hardhat/contracts/atadiaToken.sol
+++ packages/hardhat/contracts/atadiaToken.sol
@@ -4,8 +4,12 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
-contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
+contract atadiaToken is ERC20 {
+    address public owner;
+
+    constructor() ERC20("Atadia", "ATA") {
         //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        owner = msg.sender;
+        _mint(msg.sender, 2000 * 10 ** 18);
     }
 }
